c8b279dc13bb4ddf5ac45987d04a51ead8bb7cec
Pekka Okkonen <pekka.okkonen1@gmail.com>
2019-08-09 11:54:11 +0300

    Added clear button and made few changes to work correctly with current mainWidgetv2

--- js/main.js
+++ js/main.js
@@ -21,33 +21,54 @@
 			method: methord,
 			url2: url2,
 		},
-		
 	}
-	
 }
 */
+// Tarvitaan jos halutaan poistaa curleja yksittäin
+
+/*{
+	Headers : {
+		header1,
+		...
+		headerX
+	},
+	url: url,
+},*/
+
+// Nyt riittää simppelimpi muoto
+
 
 function getInfo(info) {
+	var curl = "curl -X 'GET'";
+	var headers = Object.keys(info.headers);
 
-	var names = Object.keys(info);
-	for(var i = 0; i < names.length; i++) {
-		var curl = "curl -X 'GET' -H 'Accept: application/json'";
-		var headers = info[names[i]]["Headers"];
-		var headers = Object.keys(headers);
-		for(var j = 0; j < headers.length; j++) {
-			console.log(info[names[i]]["Headers"]);
-			curl = curl + " -H '" + headers[j] + ": " + info[names[i]]["Headers"][headers[j]] +  "'";
+	for(var j = 0; j < headers.length; j++) {
+		if (headers[j] === "Accept" || headers[j] === "Platform-ApiKey" ||
+		headers[j] === "Fiware-Service" || headers[j] === "Fiware-ServicePath") {
+			// Temporary if-else to hide Apikey
+			if (headers[j] === "Platform-ApiKey") {
+				curl = curl + " -H '" + headers[j] + ": " + "***Hidden ApiKey***" +  "'";
+			} else {
+				curl = curl + " -H '" + headers[j] + ": " + info.headers[headers[j]] +  "'";
+			}
 		}
-		curl = curl + " '" + info[names[i]]["url"] + "'";
-		
-		var curlText = document.createElement("TEXTAREA");
-		curlText.innerHTML = curl;
-		console.log(MashupPlatform.widget.context.get('widthInPixels'));
-		curlText.style.width = (MashupPlatform.widget.context.get('widthInPixels') -10) + 'px';
-		document.body.appendChild(curlText);
 	}
 	
+	var curlText = document.createElement("TEXTAREA");
+	curlText.innerHTML = curl + " '" + info.url + "'";
+	curlText.style.width = (MashupPlatform.widget.context.get('widthInPixels') - 25) + "px";
+	curlText.style.height = 60 + 'px';
+	document.body.appendChild(curlText);
 }
 
+window.onload=function() {
+	var btn = document.createElement("BUTTON");
+	btn.innerHTML = "Clear widget";
+	document.body.appendChild(btn);
+	btn.onclick = async function() {
+		document.body.innerHTML = "";
+		document.body.appendChild(btn);
+	}
+}
 
 MashupPlatform.wiring.registerCallback('printCurl', function(data) {getInfo(data)});
